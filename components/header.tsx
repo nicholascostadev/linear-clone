2e387a4d5cb646e8e9d462ecae35944102ba75d9
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-22 18:28:36 -0300

    Fix: Header flickering issue

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ import { Logo } from './icons/logo'
 import classNames from 'classnames'
 
 export const Header = () => {
-  const [hamburgerMenuIsOpen, setHambugerMenuIsOpen] = useState(false)
+  const [hamburgerMenuIsOpen, setHamburgerMenuIsOpen] = useState(false)
 
   return (
     <header className="fixed top-0 left-0 w-full border-b border-white-a08 backdrop-blur-[12px]">
@@ -26,7 +26,7 @@ export const Header = () => {
         >
           <nav
             className={classNames(
-              'fixed top-navigation-height left-0 h-[calc(100vh_-_var(--navigation-height))] w-full overflow-auto bg-background transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto md:translate-x-0 md:bg-transparent md:opacity-100',
+              'fixed top-navigation-height left-0 h-[calc(100vh_-_var(--navigation-height))] w-full overflow-auto bg-background transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto md:translate-x-0 md:bg-transparent md:opacity-100 md:transition-none',
               hamburgerMenuIsOpen
                 ? 'translate-x-0 opacity-100'
                 : 'translate-x-[-100vw] opacity-0',
@@ -35,7 +35,7 @@ export const Header = () => {
             <ul
               className={classNames(
                 'flex h-full flex-col md:flex-row md:items-center [&_li]:ml-6 [&_li]:border-b [&_li]:border-grey-dark md:[&_li]:border-none',
-                'ease-in [&_a]:flex [&_a]:h-navigation-height [&_a]:w-full [&_a]:translate-y-8 [&_a]:items-center [&_a]:text-md [&_a]:transition-[color,transform] [&_a]:duration-300 md:[&_a]:translate-y-0 md:[&_a]:text-sm [&_a:hover]:text-grey',
+                'ease-in [&_a]:flex [&_a]:h-navigation-height [&_a]:w-full [&_a]:translate-y-8 [&_a]:items-center [&_a]:text-md [&_a]:transition-[color,transform] [&_a]:duration-300 md:[&_a]:translate-y-0 md:[&_a]:text-sm [&_a]:md:transition-colors [&_a:hover]:text-grey',
                 hamburgerMenuIsOpen && '[&_a]:translate-y-0',
               )}
             >
@@ -73,7 +73,7 @@ export const Header = () => {
 
         <button
           className="ml-6 md:hidden"
-          onClick={() => setHambugerMenuIsOpen((open) => !open)}
+          onClick={() => setHamburgerMenuIsOpen((open) => !open)}
         >
           <span className="sr-only">Toggle menu</span>
           <HamburgerIcon />
